4b79b4c25f22a6d5626a4ce24362e3a2c88bc8af
WhiteFang4221 <artemka7331@bk.ru>
2023-04-14 19:11:49 +0300

    1

--- Assets/Scripts/PlayerMoving.cs
+++ Assets/Scripts/PlayerMoving.cs
@@ -14,6 +14,7 @@ public static class AnimatorPlayerController
         public const string IsOnGround = "IsOnGround";
         public const string AirSpeedY = "AirSpeedY";
         public const string RollTrigger = "RollTrigger";
+        public const string IsOnWall = "IsOnWall";
 
         public static class States
         {
@@ -35,7 +36,16 @@ public class PlayerMoving : MonoBehaviour
 
     [SerializeField] private Transform _groundChecker;
     [SerializeField] private LayerMask _groundLayer;
-    private float _checkRadius = 0.5f;
+    private float _groundCheckRadius;
+    private bool _isOnGround;
+
+    [SerializeField] private Transform _wallCheckerUp;
+    [SerializeField] private Transform _wallCheckerDown;
+    [SerializeField] private LayerMask _wall;
+    [SerializeField] private float _slideSpeed = 4f;
+    private float _wallCheckRadius;
+    private bool _isOnWall;
+
 
     private Rigidbody2D _rigidbody;
     private Animator _animator;
@@ -44,30 +54,67 @@ public class PlayerMoving : MonoBehaviour
 
     private bool _isfaceRight = true;
 
-    private bool _isOnGround;
 
-    private float _rollForce = 100f;
+    private bool _isCanRoll = true;
+    private bool _isRolling;
+    private float _rollPower = 15f;
+    private float _rollingTime = 0.5f;
+    private float _rollCooldown = 1f;
+
+    private float _gravityDefault;
+
+
 
     private void Start()
     {
-        _rigidbody= GetComponent<Rigidbody2D>();
+        _groundCheckRadius = _groundChecker.GetComponent<CircleCollider2D>().radius;
+        _wallCheckRadius = _wallCheckerUp.GetComponent<CircleCollider2D>().radius;
+        _rigidbody = GetComponent<Rigidbody2D>();
         _animator= GetComponent<Animator>();
+        _gravityDefault = _rigidbody.gravityScale;
     }
 
     private void Update()
     {
-        
+        if (!_isRolling)
+        {
         Walk();
+        }
+
         Reflect();
         Jump();
         CheckGround();
+        CheckWall();
 
-        if (Input.GetKeyDown(KeyCode.LeftShift) && _isOnGround == true)
+        MoveOnWall();
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && _isOnGround && _isCanRoll)
         {
-            StartCoroutine(Roll());
+            _isCanRoll = false;
+            _isRolling = true;
+            _rigidbody.velocity = new Vector2(0, 0);
+            _animator.SetTrigger(AnimatorPlayerController.Params.RollTrigger);
+
+            if (_isfaceRight)
+            {
+                _rigidbody.AddForce(new Vector2(_rollPower * 1, 0f), ForceMode2D.Impulse);
+            }
+            else
+            {
+                _rigidbody.AddForce(new Vector2(_rollPower * (-1), 0f), ForceMode2D.Impulse);
+            }
+
+            float gravity = _rigidbody.gravityScale;
+            _rigidbody.gravityScale = 0;
+            _isRolling = false;
+            _rigidbody.gravityScale = gravity;
+            _isCanRoll = true;
         }
-
-
     }
 
     private void Walk()
@@ -88,7 +135,7 @@ public class PlayerMoving : MonoBehaviour
 
     private void Jump()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && _isOnGround == true)
+        if(Input.GetKeyDown(KeyCode.Space) && _isOnGround == true && _isRolling == false)
         {
             _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpForce);
         }
@@ -96,17 +143,53 @@ public class PlayerMoving : MonoBehaviour
 
     private IEnumerator Roll()
     {
+        _isCanRoll= false;
+        _isRolling = true;
+        _rigidbody.velocity = new Vector2(0,0);
+        _animator.SetTrigger(AnimatorPlayerController.Params.RollTrigger);
 
-            _animator.SetTrigger(AnimatorPlayerController.Params.RollTrigger);
-            _speed *= 3;
-        yield return new WaitForSeconds(0.5f);
-        _speed = 5f;
+        if (_isfaceRight)
+        {
+            _rigidbody.AddForce(new Vector2(_rollPower * 1, 0f), ForceMode2D.Impulse);
+        }
+        else
+        {
+            _rigidbody.AddForce(new Vector2(_rollPower * (-1), 0f), ForceMode2D.Impulse);
+        }
+
+        float gravity = _rigidbody.gravityScale;
+        _rigidbody.gravityScale = 0;
+        yield return new WaitForSeconds(_rollingTime);
+        _isRolling = false;
+        _rigidbody.gravityScale = gravity;
+        yield return new WaitForSeconds(_rollCooldown);
+        _isCanRoll= true;
     }
 
     private void CheckGround()
     {
-        _isOnGround = Physics2D.OverlapCircle(_groundChecker.position, _checkRadius, _groundLayer);
+        _isOnGround = Physics2D.OverlapCircle(_groundChecker.position, _groundCheckRadius, _groundLayer);
         _animator.SetBool (AnimatorPlayerController.Params.IsOnGround, _isOnGround);
         _animator.SetFloat(AnimatorPlayerController.Params.AirSpeedY, _rigidbody.velocity.y);
+    }
+    
+    private void CheckWall()
+    {
+        _isOnWall = Physics2D.OverlapCircle(_wallCheckerUp.position, _wallCheckRadius, _wall) && Physics2D.OverlapCircle(_wallCheckerDown.position, _wallCheckRadius, _wall);
+    }
+
+    private void MoveOnWall()
+    {
+        if (_isOnWall && !_isOnGround) 
+        {
+            _rigidbody.gravityScale = 0;
+            _rigidbody.velocity = new Vector2(0, -_slideSpeed);
+            _animator.SetBool(AnimatorPlayerController.Params.IsOnWall, true);
+        }
+        else if (!_isOnWall || _isOnGround)
+        {
+            _rigidbody.gravityScale = _gravityDefault;
+            _animator.SetBool(AnimatorPlayerController.Params.IsOnWall, false);
+        }
     }
 }
